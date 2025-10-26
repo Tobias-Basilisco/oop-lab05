e054a75813930f5d5c50d9c8a1a0ce0d4f7390ff
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-10-26 19:40:51 +0100

    productImpl

--- 53-implement-with-collections/src/main/java/it/unibo/collections/design/impl/ProductImpl.java
+++ 53-implement-with-collections/src/main/java/it/unibo/collections/design/impl/ProductImpl.java
@@ -3,19 +3,32 @@ package it.unibo.collections.design.impl;
 import it.unibo.collections.design.api.Product;
 
 public class ProductImpl implements Product{
+    private static int DEFAULT_QUANTITY = 1;
+
     private String name;
+    private int quantity;
+
+    public ProductImpl(final String name){
+        this.name = name;
+        this.quantity = DEFAULT_QUANTITY;
+    }
+
+    public ProductImpl(final String name, final int quantity){
+        this.name = name;
+        this.quantity = quantity;
+    }
 
     /**
      * @return the product name
      */
     public String getName(){
-        return null;
+        return name;
     }
 
     /**
      * @return the amount of product
      */
     public double getQuantity(){
-        return 0;
+        return quantity;
     }
 }
